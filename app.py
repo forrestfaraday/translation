821762330d4cb000a98f730f38b3cd1a2d7262b6
forrestfaraday <87153193+forrestfaraday@users.noreply.github.com>
2023-04-06 22:28:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-from transformers import MarianMTModel, MarianTokenizer
+from transformers import MarianMTModel, MarianTokenizer, pipeline
 
 # Opus-MT modeli için tokenizer ve modeli yükleyin
 tokenizer = MarianTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-es")
 model = MarianMTModel.from_pretrained("Helsinki-NLP/opus-mt-en-es")
-
+pipe = pipeline("translation_en_to_es, tokenizer=tokenizer,model=model, device=0)
 # Streamlit uygulamasının başlığını ayarlayın
 st.title("Opus-MT Çeviri")
 
@@ -14,9 +14,7 @@ text = st.text_area("Metni girin", value='', height=200)
 # Çeviriyi gerçekleştirin
 if st.button("Çevir"):
     if text:
-        inputs = tokenizer.encode(text, return_tensors="pt")
-        translated = model.generate(inputs["input_ids"], max_length=128, num_return_sequences=1)
-        translated_text = tokenizer.decode(translated[0], skip_special_tokens=True)
+        translated_text = pipe(text)[0]['translation_text]
         st.write("Çeviri Sonucu:")
         st.write(translated_text)
     else:
